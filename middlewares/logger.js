a40ac9df7137ed5a9e96da38e7224e916b22d1ef
7odadocoding <mahmoud.alnakeeb.js@gmail.com>
2023-05-16 22:58:40 +0300

    logger error with vercel

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -3,10 +3,13 @@ const winston = require('winston');
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: 'logs.log', dirname: './logs' }),
-  ],
+  transports:
+    process.env.NODE_ENV == 'development'
+      ? [
+          new winston.transports.Console(),
+          new winston.transports.File({ filename: 'logs.log', dirname: './logs' }),
+        ]
+      : [new winston.transports.Console()],
 });
 
 module.exports = logger;
