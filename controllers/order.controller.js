17ad2e8d38cb03115990766515874115a7aecd38
7odadocoding <mahmoud.alnakeeb.js@gmail.com>
2023-07-31 14:14:48 +0300

    getOrderById , getOrderById and getOrders done

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,4 +1,5 @@
 const { InternalError } = require('../middlewares/errorhandler');
+const Order = require('../models/Order');
 const orderService = require('../services/orders');
 const Responser = require('../utils/responser');
 
@@ -15,4 +16,38 @@ const createOrder = async (req, res, next) => {
   }
 };
 
-module.exports = { createOrder };
+const getOrderById = async (req, res) => {
+  try {
+    let { id } = req.params;
+    let order = await Order.findById(id).lean();
+    let responser = new Responser(201, 'order fetched successfully', order);
+    responser.respond(res);
+  } catch (error) {
+    next(new InternalError('Internal error', error));
+  }
+};
+
+const getUserOrders = async (req, res) => {
+  try {
+    let { limit, page } = req.query;
+    let skip = (page - 1) * limit;
+    let orders = await Order.find({ userId: req.userId }).skip(skip).limit(limit).lean();
+    let responser = new Responser(201, 'user orders fetched successfully', orders);
+    responser.respond(res);
+  } catch (error) {
+    next(new InternalError('Internal error', error));
+  }
+};
+
+const getOrders = async (req, res) => {
+  try {
+    let { limit, page } = req.query;
+    let skip = (page - 1) * limit;
+    let orders = await Order.find().skip(skip).limit(limit).lean();
+    let responser = new Responser(201, 'orders fetched successfully', orders);
+    responser.respond(res);
+  } catch (error) {
+    next(new InternalError('Internal error', error));
+  }
+};
+module.exports = { createOrder, getOrderById, getUserOrders, getOrders };
--- routes/order.router.js
+++ routes/order.router.js
@@ -1,11 +1,13 @@
-const { createOrder } = require('../controllers/order.controller');
+const { createOrder, getOrders, getOrderById, getUserOrders } = require('../controllers/order.controller');
 const authMiddleware = require('../middlewares/authentication');
 const authorize = require('../middlewares/authorization');
 const orderRouter = require('express').Router();
 
 orderRouter.post('/', authMiddleware, createOrder);
+orderRouter.get('/user', authMiddleware, getUserOrders);
 
 // get orders [dashboard]
-orderRouter.get('/', authMiddleware, authorize('ADMIN'));
+orderRouter.get('/', authMiddleware, authorize('ADMIN'), getOrders);
+orderRouter.get('/:id', authMiddleware, authorize('ADMIN'), getOrderById);
 
 module.exports = orderRouter;
