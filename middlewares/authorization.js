c3b865b204e35593cfd39f8b62ace39e6f0d5386
7odadocoding <mahmoud.alnakeeb.js@gmail.com>
2023-08-11 03:26:17 +0300

    moderator full access in any situation

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -10,10 +10,11 @@ const Responser = require('../utils/responser');
 const authorize = (role) => {
   return async (req, res, next) => {
     let user = await User.findById(req.userId);
-    if (user.role != role) {
+    if (user.role == 'MODERATOR') return next();
+    else if (user.role != role) {
       let responser = new Responser(401, 'you are not authorized to do this action');
       return responser.respond(res);
-    } else if (user.role == 'MODERATOR') return next();
+    }
     next();
   };
 };
